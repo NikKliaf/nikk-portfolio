99afbb077a9ee37f1774d1d7e0d06d7ea401e7e6
NikKliaf <nik.kliaf1@gmail.com>
2025-12-04 03:11:57 +0200

    Implement Hero and LanguageToggle components; Update localization files for multilingual support

---
+++ components/Hero.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from 'react'
+import { useTranslation } from '@/components/TranslationProvider';
+
+const Hero = () => {
+
+  const { t } = useTranslation();
+
+  return (
+    <div className="text-center h-full justify-center">
+        <h1>
+            {t('hero.title')}
+        </h1>
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/LanguageToggle.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useRouter, usePathname } from "next/navigation";
+import { i18n } from "@/il8n.config";
+import path from "path";
+
+const LANGUAGES = [
+    { code: "en", label: "🇬🇧" },
+    { code: "el", label: "🇬🇷" },
+]
+
+export const LanguageToggle = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+
+    const pathSegments = pathname.split('/');
+    const currentLocale = pathSegments[1] && i18n.locales.includes(pathSegments[1]) 
+        ? pathSegments[1] 
+        : i18n.defaultLocale;
+
+    const changeLanguage = (newLocale: string) => {
+        if (newLocale == currentLocale) return;
+
+        let newPathName = pathname;
+
+        if(i18n.locales.includes(currentLocale)) {
+            newPathName = pathname.replace(`/${currentLocale}`, `/${newLocale}`);
+        } else {
+            newPathName = `/${newLocale}${pathname}`;
+        }
+        
+        router.replace(newPathName);
+    };
+
+    return (
+        <div className="fixed bottom-6 right-6 z-[999] flex items-center bg-black/20 backdrop-blur-sm rounded-full p-1 border border-white/10">
+            {LANGUAGES.map(({code, label}) => {
+                const isActive = code === currentLocale;
+                
+                return (
+                    <button 
+                        key={code}
+                        onClick={() => changeLanguage(code)}
+                        className= {`
+                            px-4 py-2 rounded-full font-semibold transition-all duration-300 text-sm 
+                            ${isActive 
+                                ? 'bg-white text-black shadow-md' 
+                                : 'text-white/70 hover:bg-white/10 hover:text-white' 
+                            }
+                        `}
+                    >
+                        {label}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
